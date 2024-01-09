f5763f344e9d0d1d7cca5b2deae36366362ee508
MarineVega <marinev33@hotmail.com>
2024-01-09 19:05:22 -0300

    subida con paginado y filtros

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // elementos del HTML
 let divPersonajes = document.getElementById('personajes');
+let divTotalPersonajes = document.getElementById('totalPersonajes');
+
     // botones del filtro
 let botonFiltroTodo = document.getElementById('filtroTodo');
 let botonFiltroMujer = document.getElementById('filtroMujer');
 let botonFiltroHombre = document.getElementById('filtroHombre');
+let botonFiltroNeutro = document.getElementById('filtroNeutro');
+let botonFiltroDesconocido = document.getElementById('filtroDesconocido');
+
     // bptones de paginadp
 let botonPrimeraPagina = document.getElementById('primeraPagina');
 let botonAnterioPagina = document.getElementById('anteriorPagina');
@@ -15,13 +20,21 @@ let paginaActual = 1;
 
 // Función para mostrar los personajes en el html
 function mostraEnHtml (arrPersonajes) { 
+    divTotalPersonajes.innerHTML = `<div>
+                                        Total de personajes: ${arrPersonajes.length} 
+                                    </div>`
+
     // limpio lo que tenía el div de personajes
     divPersonajes.innerHTML = '';   
     arrPersonajes.forEach((itemPersonaje) => {
         divPersonajes.innerHTML +=  `<div class="personaje">
-                                        <h3>Nombre: ${itemPersonaje.name}</h3>
-                                        <p>Género: ${itemPersonaje.gender}</p>
+                                        <h3>${itemPersonaje.name}</h3>
                                         <img src=${itemPersonaje.image}>
+                                        <p>Género: ${itemPersonaje.gender}</p>
+                                        <p>Especie: ${itemPersonaje.species}</p>
+                                        <p>Estado: ${itemPersonaje.status}</p>
+                                        <p>Origen: ${itemPersonaje.origin.name}</p>
+                                        <p>Locación: ${itemPersonaje.location.name}</p>
                                     </div>`
     })
 }
@@ -50,6 +63,10 @@ controlPaginado(paginaActual);
 
 // Funciones para el filtro
 //('Female', 'Male', 'Genderless' or 'unknown')
+function filtroTodo () {    
+    mostraEnHtml(totalPersonajes)
+};
+
 function filtroMujer () {
     let mujeres = totalPersonajes.filter((itemPersonaje)=>{
         return itemPersonaje.gender==='Female';
@@ -64,36 +81,60 @@ function filtroHombre () {
     mostraEnHtml(hombres)
 };
 
-function filtroTodo () {    
-    mostraEnHtml(totalPersonajes)
+function filtroNeutro () {
+    let neutros = totalPersonajes.filter((itemPersonaje)=>{
+        return itemPersonaje.gender==='Genderless';
+    });
+    mostraEnHtml(neutros)
 };
+
+function filtroDesconocido () {
+    let desconocidos = totalPersonajes.filter((itemPersonaje)=>{
+        return itemPersonaje.gender==='unknown';
+    });
+    mostraEnHtml(desconocidos)
+};
+
 
 // Crear evento
 // elementoHTML.addEventListener('tipo de evento', función que se ejecuta cuando se da el evento)
+botonFiltroTodo.addEventListener('click',filtroTodo)
 botonFiltroMujer.addEventListener('click',filtroMujer)
 botonFiltroHombre.addEventListener('click',filtroHombre)
-botonFiltroTodo.addEventListener('click',filtroTodo)
+botonFiltroNeutro.addEventListener('click',filtroNeutro)
+botonFiltroDesconocido.addEventListener('click',filtroDesconocido)
 
 function controlPaginado (pagina) {
     // agregar los controles de todas las situaciones
 
     if(pagina===1){
         botonPrimeraPagina.disabled=true;
-        botonAnterioPagina.disabled=true;
+        botonPrimeraPagina.className="disable";
+        botonAnterioPagina.disabled=true;       
+        botonAnterioPagina.className="disable";
         botonSiguientePagina.disabled=false;
+        botonSiguientePagina.classList.remove("disable");
         botonUltimaPagina.disabled=false;
+        botonUltimaPagina.classList.remove("disable");
     } else {
         if(paginaActual===42){            
             botonPrimeraPagina.disabled=false;
+            botonPrimeraPagina.classList.remove("disable");
             botonAnterioPagina.disabled=false;
+            botonAnterioPagina.classList.remove("disable");
             botonSiguientePagina.disabled=true;
+            botonSiguientePagina.className="disable";
             botonUltimaPagina.disabled=true;
-
+            botonUltimaPagina.className="disable";
         } else {
             botonPrimeraPagina.disabled=false;
+            botonPrimeraPagina.classList.remove("disable");
             botonAnterioPagina.disabled=false;
-            botonAnterioPagina.disabled=false;
-            botonPrimeraPagina.disabled=false;    
+            botonAnterioPagina.classList.remove("disable");
+            botonSiguientePagina.disabled=false;
+            botonSiguientePagina.classList.remove("disable");
+            botonUltimaPagina.disabled=false;
+            botonUltimaPagina.classList.remove("disable");
         }
     }
 };
